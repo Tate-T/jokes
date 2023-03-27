6cc7c787faa30e13d1f7eb8a34a1a9cb05d233e8
Tetiana Tarasovych <86785655+Tate-T@users.noreply.github.com>
2023-03-28 00:03:03 +0200

    store

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {jokesApi} from './jokesApi';
+
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+export const store = configureStore({
+    reducer: {
+        [jokesApi.reducerPath]: jokesApi.reducer,
+        filter: filterReducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(jokesApi.middleware);
+    },
+    devTools: process.env.NODE_ENV !== 'production', // true
+});
+setupListeners(store.dispatch)
