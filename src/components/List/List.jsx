e4fa04ceba48a1f1246b4c6e48149767b5dd4d48
Tetiana Tarasovych <86785655+Tate-T@users.noreply.github.com>
2023-03-28 16:19:48 +0200

    @mui/material/Button

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,21 +1,26 @@
 import s from './List.module.css';
 import React, { useEffect } from 'react';
-
+import Button from '@mui/material/Button';
 import { useDispatch, useSelector } from 'react-redux';
-import { addJoke, deleteJoke, requestJokes, requestRefreshJokes } from '../../redux/jokesReducer';
+import {
+  addJoke,
+  deleteJoke,
+  requestJokes,
+  requestRefreshJokes,
+} from '../../redux/jokesReducer';
 
 export const List = ({ filter }) => {
   //const { data, error, isLoading } = useGetJokesQuery();
-//   console.log('data: ', data);
-    const jokes = useSelector((state) => state.jokes.jokes);
-    const favJokes = useSelector((state) => state.jokes.favJokes)
-    const isLoading = useSelector((state) => state.jokes.isLoading);
-    const dispatch = useDispatch();
- // const [deleteJoke] = useDeleteJokeMutation();
+  //   console.log('data: ', data);
+  const jokes = useSelector(state => state.jokes.jokes);
+  const favJokes = useSelector(state => state.jokes.favJokes);
+  const isLoading = useSelector(state => state.jokes.isLoading);
+  const dispatch = useDispatch();
+  // const [deleteJoke] = useDeleteJokeMutation();
 
- useEffect(() => {
+  useEffect(() => {
     dispatch(requestJokes());
- }, [dispatch])
+  }, [dispatch]);
 
   // const findJokes = () => {
   //     if (!filter) {
@@ -26,286 +31,65 @@ export const List = ({ filter }) => {
 
   const finalJokes = [...favJokes, ...jokes];
   return (
-    <ul className={s.jokesList}>
+    <>
+      <ul className={s.jokesList}>
+        {finalJokes?.length > 0 &&
+          finalJokes.map(joke => (
+            <li className={s.jokesCard}>
+              <div className={s.jokesContent}>
+                <p className={s.jokesType}>
+                  Type: <span className={s.jokesName}>{joke.type}</span>
+                </p>
+                <p className={s.jokesId}>ID: {joke.id}</p>
+              </div>
+              <h3 className={s.jokesSetup}>Setup:</h3>
+              <p className={s.jokesText}>{joke.setup}</p>
+              <h3 className={s.jokesPunchline}>Punchline:</h3>
+              <p className={s.jokesText}>{joke.punchline}</p>
+              <ul className={s.jokesBtns}>
+                <li className={s.jokesBtn}>
+                  <button
+                    onClick={() => dispatch(deleteJoke(joke))}
+                    className={s.jokesDel}
+                    type="button"
+                  >
+                    Delete
+                  </button>
+                </li>
+                <li className={s.jokesBtn}>
+                  <button
+                    onClick={() => dispatch(addJoke(joke))}
+                    className={s.jokesAdd}
+                    type="button"
+                  >
+                    Add
+                  </button>
+                </li>
+                <li className={s.jokesBtn}>
+                  <button
+                    onClick={() => {
+                      dispatch(deleteJoke(joke));
+                      dispatch(requestRefreshJokes());
+                    }}
+                    className={s.jokesRefresh}
+                    type="button"
+                  >
+                    Refresh
+                  </button>
+                </li>
+              </ul>
+            </li>
+          ))}
+      </ul>
 
-      {finalJokes?.length > 0 && finalJokes.map(joke => (
-                <li className={s.jokesCard}>
-                <div className={s.jokesContent}>
-                  <p className={s.jokesType}>
-                    Type: <span className={s.jokesName}>{joke.type}</span>
-                  </p>
-                  <p className={s.jokesId}>ID: {joke.id}</p>
-                </div>
-                <h3 className={s.jokesSetup}>Setup:</h3>
-                <p className={s.jokesText}>{joke.setup}</p>
-                <h3 className={s.jokesPunchline}>Punchline:</h3>
-                <p className={s.jokesText}>{joke.punchline}</p>
-                <ul className={s.jokesBtns}>
-                  <li className={s.jokesBtn}>
-                    <button onClick={()=> dispatch(deleteJoke(joke))} className={s.jokesDel} type="button">
-                      Delete
-                    </button>
-                  </li>
-                  <li className={s.jokesBtn}>
-                    <button onClick={()=> dispatch(addJoke(joke))} className={s.jokesAdd} type="button">
-                      Add
-                    </button>
-                  </li>
-                  <li className={s.jokesBtn}>
-                    <button onClick={()=> {
-                        dispatch(deleteJoke(joke))
-                        dispatch(requestRefreshJokes())
-                        }}  className={s.jokesRefresh} type="button">
-                      Refresh
-                    </button>
-                  </li>
-                </ul>
-              </li>
-            ))
-
-            }
-
-      {/* <li className={s.jokesCard}>
-        <div className={s.jokesContent}>
-          <p className={s.jokesType}>
-            Type: <span className={s.jokesName}>General</span>
-          </p>
-          <p className={s.jokesId}>ID: #12</p>
-        </div>
-        <h3 className={s.jokesSetup}>Setup:</h3>
-        <p className={s.jokesText}>text</p>
-        <h3 className={s.jokesPunchline}>Punchline:</h3>
-        <p className={s.jokesText}>text</p>
-        <ul className={s.jokesBtns}>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesDel} type="button">
-              Delete
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesAdd} type="button">
-              Add
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesRefresh} type="button">
-              Refresh
-            </button>
-          </li>
-        </ul>
-      </li>
-
-      <li className={s.jokesCard}>
-        <div className={s.jokesContent}>
-          <p className={s.jokesType}>
-            Type: <span className={s.jokesName}>General</span>
-          </p>
-          <p className={s.jokesId}>ID: #12</p>
-        </div>
-        <h3 className={s.jokesSetup}>Setup:</h3>
-        <p className={s.jokesText}>text</p>
-        <h3 className={s.jokesPunchline}>Punchline:</h3>
-        <p className={s.jokesText}>text</p>
-        <ul className={s.jokesBtns}>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesDel} type="button">
-              Delete
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesAdd} type="button">
-              Add
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesRefresh} type="button">
-              Refresh
-            </button>
-          </li>
-        </ul>
-      </li>
-
-      <li className={s.jokesCard}>
-        <div className={s.jokesContent}>
-          <p className={s.jokesType}>
-            Type: <span className={s.jokesName}>General</span>
-          </p>
-          <p className={s.jokesId}>ID: #12</p>
-        </div>
-        <h3 className={s.jokesSetup}>Setup:</h3>
-        <p className={s.jokesText}>text</p>
-        <h3 className={s.jokesPunchline}>Punchline:</h3>
-        <p className={s.jokesText}>text</p>
-        <ul className={s.jokesBtns}>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesDel} type="button">
-              Delete
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesAdd} type="button">
-              Add
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesRefresh} type="button">
-              Refresh
-            </button>
-          </li>
-        </ul>
-      </li>
-
-      <li className={s.jokesCard}>
-        <div className={s.jokesContent}>
-          <p className={s.jokesType}>
-            Type: <span className={s.jokesName}>General</span>
-          </p>
-          <p className={s.jokesId}>ID: #12</p>
-        </div>
-        <h3 className={s.jokesSetup}>Setup:</h3>
-        <p className={s.jokesText}>text</p>
-        <h3 className={s.jokesPunchline}>Punchline:</h3>
-        <p className={s.jokesText}>text</p>
-        <ul className={s.jokesBtns}>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesDel} type="button">
-              Delete
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesAdd} type="button">
-              Add
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesRefresh} type="button">
-              Refresh
-            </button>
-          </li>
-        </ul>
-      </li>
-
-      <li className={s.jokesCard}>
-        <div className={s.jokesContent}>
-          <p className={s.jokesType}>
-            Type: <span className={s.jokesName}>General</span>
-          </p>
-          <p className={s.jokesId}>ID: #12</p>
-        </div>
-        <h3 className={s.jokesSetup}>Setup:</h3>
-        <p className={s.jokesText}>text</p>
-        <h3 className={s.jokesPunchline}>Punchline:</h3>
-        <p className={s.jokesText}>text</p>
-        <ul className={s.jokesBtns}>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesDel} type="button">
-              Delete
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesAdd} type="button">
-              Add
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesRefresh} type="button">
-              Refresh
-            </button>
-          </li>
-        </ul>
-      </li>
-
-      <li className={s.jokesCard}>
-        <div className={s.jokesContent}>
-          <p className={s.jokesType}>
-            Type: <span className={s.jokesName}>General</span>
-          </p>
-          <p className={s.jokesId}>ID: #12</p>
-        </div>
-        <h3 className={s.jokesSetup}>Setup:</h3>
-        <p className={s.jokesText}>text</p>
-        <h3 className={s.jokesPunchline}>Punchline:</h3>
-        <p className={s.jokesText}>text</p>
-        <ul className={s.jokesBtns}>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesDel} type="button">
-              Delete
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesAdd} type="button">
-              Add
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesRefresh} type="button">
-              Refresh
-            </button>
-          </li>
-        </ul>
-      </li>
-
-      <li className={s.jokesCard}>
-        <div className={s.jokesContent}>
-          <p className={s.jokesType}>
-            Type: <span className={s.jokesName}>General</span>
-          </p>
-          <p className={s.jokesId}>ID: #12</p>
-        </div>
-        <h3 className={s.jokesSetup}>Setup:</h3>
-        <p className={s.jokesText}>text</p>
-        <h3 className={s.jokesPunchline}>Punchline:</h3>
-        <p className={s.jokesText}>text</p>
-        <ul className={s.jokesBtns}>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesDel} type="button">
-              Delete
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesAdd} type="button">
-              Add
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesRefresh} type="button">
-              Refresh
-            </button>
-          </li>
-        </ul>
-      </li>
-
-      <li className={s.jokesCard}>
-        <div className={s.jokesContent}>
-          <p className={s.jokesType}>
-            Type: <span className={s.jokesName}>General</span>
-          </p>
-          <p className={s.jokesId}>ID: #12</p>
-        </div>
-        <h3 className={s.jokesSetup}>Setup:</h3>
-        <p className={s.jokesText}>text</p>
-        <h3 className={s.jokesPunchline}>Punchline:</h3>
-        <p className={s.jokesText}>text</p>
-        <ul className={s.jokesBtns}>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesDel} type="button">
-              Delete
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesAdd} type="button">
-              Add
-            </button>
-          </li>
-          <li className={s.jokesBtn}>
-            <button className={s.jokesRefresh} type="button">
-              Refresh
-            </button>
-          </li>
-        </ul>
-      </li> */}
-      <button onClick={() => dispatch(requestRefreshJokes())}>Load more</button>
-    </ul>
+      <Button
+        variant="contained"
+        onClick={() => dispatch(requestRefreshJokes())}
+        className={s.jokesLoad}
+      >
+        Load more
+      </Button>
+    </>
   );
 };
 
